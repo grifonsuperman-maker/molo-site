10e0ed30d0b4ab4c0540b17b08ca68cbda914518
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-05-30 20:23:24 +0300

    Add update restaurant dto

---
+++ backend/src/restaurant/dto/update-restaurant.dto.ts
@@ -0,0 +1,47 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class UpdateRestaurantDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  menuUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  logoUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  mainPhotoUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  openTime?: string;
+
+  @IsOptional()
+  @IsString()
+  bookingCloseTime?: string;
+
+  @IsOptional()
+  @IsString()
+  closeTime?: string;
+
+  @IsOptional()
+  @IsString()
+  closeMessage?: string;
+
+  @IsOptional()
+  @IsString()
+  bookingClosedMessage?: string;
+}
