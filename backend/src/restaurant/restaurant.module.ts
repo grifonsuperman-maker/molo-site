a00216bd9a6e1ed85d6360a238267815b7380ca5
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-05-30 21:23:46 +0300

    Add restaurant module

---
+++ backend/src/restaurant/restaurant.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantController } from './restaurant.controller';
+import { RestaurantService } from './restaurant.service';
+import { LogsModule } from '../logs/logs.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Restaurant]), LogsModule],
+  controllers: [RestaurantController],
+  providers: [RestaurantService],
+  exports: [RestaurantService],
+})
+export class RestaurantModule {}
