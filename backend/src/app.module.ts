0a5ffa56b808f54f213898e8a34bd4d63a92b60a
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-06-01 08:09:52 +0300

    Create database tables automatically

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { ZonesModule } from './zones/zones.module';
 import { TablesModule } from './tables/tables.module';
@@ -15,24 +16,31 @@ import { ConstructorModule } from './constructor/constructor.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { BroadcastsModule } from './broadcasts/broadcasts.module';
 import { TelegramModule } from './telegram/telegram.module';
+import { SchedulesModule } from './schedules/schedules.module';
+
 import { RolesGuard } from './auth/guards/roles.guard';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { AuthModule } from './auth/auth.module';
-import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
     ScheduleModule.forRoot(),
+
     TypeOrmModule.forRoot(
       process.env.DB_URL
         ? {
             type: 'postgres',
             url: process.env.DB_URL,
             ssl: { rejectUnauthorized: false },
-            extra: { ssl: { rejectUnauthorized: false } },
+            extra: {
+              ssl: { rejectUnauthorized: false },
+            },
             autoLoadEntities: true,
-            synchronize: false,
+
+            // ВРЕМЕННО: создаёт таблицы в базе автоматически
+            synchronize: true,
           }
         : {
             type: 'postgres',
@@ -42,9 +50,12 @@ import { SchedulesModule } from './schedules/schedules.module';
             password: process.env.DB_PASSWORD || 'postgres',
             database: process.env.DB_NAME || 'molo_restaurant',
             autoLoadEntities: true,
-            synchronize: false,
+
+            // ВРЕМЕННО: создаёт таблицы в базе автоматически
+            synchronize: true,
           },
     ),
+
     AuthModule,
     LogsModule,
     NotificationsModule,
@@ -60,9 +71,16 @@ import { SchedulesModule } from './schedules/schedules.module';
     TelegramModule,
     SchedulesModule,
   ],
+
   providers: [
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}
