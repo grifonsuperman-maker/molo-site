6636e450ecbcddb48e32828317a9a1558108b405
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-06-01 06:34:56 +0300

    Make constructor public temporarily

--- backend/src/constructor/constructor.controller.ts
+++ backend/src/constructor/constructor.controller.ts
@@ -5,13 +5,12 @@ import { UpdateSizeDto } from './dto/update-size.dto';
 import { ExpandMapDto } from './dto/expand-map.dto';
 import { CreateMapObjectDto } from './dto/create-map-object.dto';
 import { Public } from '../common/decorators/public.decorator';
-import { Roles } from '../common/decorators/roles.decorator';
 
-@Roles('owner', 'admin')
 @Controller('constructor')
 export class ConstructorController {
   constructor(private readonly service: ConstructorService) {}
 
+  @Public()
   @Get('map')
   getFullMap() {
     return this.service.getFullMap();
@@ -23,76 +22,91 @@ export class ConstructorController {
     return this.service.getPublicMap();
   }
 
+  @Public()
   @Patch('tables/:id/position')
   updateTablePosition(@Param('id') id: string, @Body() dto: UpdatePositionDto) {
     return this.service.updateTablePosition(id, dto);
   }
 
+  @Public()
   @Patch('tables/:id/size')
   updateTableSize(@Param('id') id: string, @Body() dto: UpdateSizeDto) {
     return this.service.updateTableSize(id, dto);
   }
 
+  @Public()
   @Patch('tables/:id/hide')
   hideTable(@Param('id') id: string) {
     return this.service.setTableVisibility(id, false);
   }
 
+  @Public()
   @Patch('tables/:id/show')
   showTable(@Param('id') id: string) {
     return this.service.setTableVisibility(id, true);
   }
 
+  @Public()
   @Patch('zones/:id/position')
   updateZonePosition(@Param('id') id: string, @Body() dto: UpdatePositionDto) {
     return this.service.updateZonePosition(id, dto);
   }
 
+  @Public()
   @Patch('zones/:id/size')
   updateZoneSize(@Param('id') id: string, @Body() dto: UpdateSizeDto) {
     return this.service.updateZoneSize(id, dto);
   }
 
+  @Public()
   @Patch('zones/:id/hide')
   hideZone(@Param('id') id: string) {
     return this.service.setZoneVisibility(id, false);
   }
 
+  @Public()
   @Patch('zones/:id/show')
   showZone(@Param('id') id: string) {
     return this.service.setZoneVisibility(id, true);
   }
 
+  @Public()
   @Post('objects')
   createObject(@Body() dto: CreateMapObjectDto) {
     return this.service.createObject(dto);
   }
 
+  @Public()
   @Patch('objects/:id/position')
   updateObjectPosition(@Param('id') id: string, @Body() dto: UpdatePositionDto) {
     return this.service.updateObjectPosition(id, dto);
   }
 
+  @Public()
   @Patch('objects/:id/size')
   updateObjectSize(@Param('id') id: string, @Body() dto: UpdateSizeDto) {
     return this.service.updateObjectSize(id, dto);
   }
 
+  @Public()
   @Patch('objects/:id/hide')
   hideObject(@Param('id') id: string) {
     return this.service.setObjectVisibility(id, false);
   }
 
+  @Public()
   @Patch('objects/:id/show')
   showObject(@Param('id') id: string) {
     return this.service.setObjectVisibility(id, true);
   }
 
+  @Public()
   @Delete('objects/:id')
   removeObject(@Param('id') id: string) {
     return this.service.removeObject(id);
   }
 
+  @Public()
   @Post('map/expand')
   expandMap(@Body() dto: ExpandMapDto) {
     return this.service.expandMap(dto);
