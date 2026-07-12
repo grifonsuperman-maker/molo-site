318ef3aea523609c3cbdf4ed12f5f053238a88ca
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-12 11:12:21 +0300

    Add TableEntity to ZonesModule imports

--- backend/src/zones/zones.module.ts
+++ backend/src/zones/zones.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Zone } from './entities/zone.entity';
 import { Restaurant } from '../restaurant/entities/restaurant.entity';
+import { TableEntity } from '../tables/entities/table.entity';
 import { ZonesController } from './zones.controller';
 import { ZonesService } from './zones.service';
-@Module({ imports:[TypeOrmModule.forFeature([Zone, Restaurant])], controllers:[ZonesController], providers:[ZonesService], exports:[ZonesService] })
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Zone, Restaurant, TableEntity])],
+  controllers: [ZonesController],
+  providers: [ZonesService],
+  exports: [ZonesService],
+})
 export class ZonesModule {}
