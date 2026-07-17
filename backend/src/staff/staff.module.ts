07acc4617f4d071c61e968d0b5034a8c370db0dd
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-17 14:04:21 +0300

    Add StaffShiftEvent to StaffModule imports

--- backend/src/staff/staff.module.ts
+++ backend/src/staff/staff.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Staff } from './entities/staff.entity';
+import { StaffShiftEvent } from './entities/staff-shift-event.entity';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
-@Module({ imports:[TypeOrmModule.forFeature([Staff])], controllers:[StaffController], providers:[StaffService], exports:[StaffService] })
+
+@Module({ imports: [TypeOrmModule.forFeature([Staff, StaffShiftEvent])], controllers: [StaffController], providers: [StaffService], exports: [StaffService], })
 export class StaffModule {}
