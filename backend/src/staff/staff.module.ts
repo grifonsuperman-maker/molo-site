af15d85c9b640c514bbdef17ea63ce855ffe0e16
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-17 15:46:29 +0300

    Add AuthModule to StaffModule imports

--- backend/src/staff/staff.module.ts
+++ backend/src/staff/staff.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { Staff } from './entities/staff.entity';
 import { StaffShiftEvent } from './entities/staff-shift-event.entity';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
 
-@Module({ imports: [TypeOrmModule.forFeature([Staff, StaffShiftEvent])], controllers: [StaffController], providers: [StaffService], exports: [StaffService], })
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Staff, StaffShiftEvent]),
+    AuthModule,
+  ],
+  controllers: [StaffController],
+  providers: [StaffService],
+  exports: [StaffService],
+})
 export class StaffModule {}
