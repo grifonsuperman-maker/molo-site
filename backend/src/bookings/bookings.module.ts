6c32526137933ccfa0a6ab91090257a4ba87e47e
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-24 01:53:40 +0300

    Connect review responses to Director permissions

--- backend/src/bookings/bookings.module.ts
+++ backend/src/bookings/bookings.module.ts
@@ -5,6 +5,7 @@ import { Client } from '../clients/entities/client.entity';
 import { LogsModule } from '../logs/logs.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { Restaurant } from '../restaurant/entities/restaurant.entity';
+import { RestaurantModule } from '../restaurant/restaurant.module';
 import { TableEntity } from '../tables/entities/table.entity';
 import { WaiterCallsModule } from '../waiter-calls/waiter-calls.module';
 import { Zone } from '../zones/entities/zone.entity';
@@ -40,6 +41,7 @@ import { GuestReviewsController } from './guest-reviews.controller';
     LogsModule,
     NotificationsModule,
     WaiterCallsModule,
+    RestaurantModule,
   ],
   controllers: [
     BookingsController,
