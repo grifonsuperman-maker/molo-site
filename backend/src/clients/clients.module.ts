3ce5990b07fe76a201eaf2ac7f18d3a3bfb11ba8
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-24 02:57:43 +0300

    Wire blacklist audit logging

--- backend/src/clients/clients.module.ts
+++ backend/src/clients/clients.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { LogsModule } from '../logs/logs.module';
 import { RestaurantModule } from '../restaurant/restaurant.module';
-import { Client } from './entities/client.entity';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { Client } from './entities/client.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Client]), RestaurantModule],
+  imports: [TypeOrmModule.forFeature([Client]), LogsModule, RestaurantModule],
   controllers: [ClientsController],
   providers: [ClientsService],
   exports: [ClientsService],
