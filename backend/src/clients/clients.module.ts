bd4c7c6e9e94e5002b6a2e48754279f46761b47e
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-24 01:48:50 +0300

    Connect client controls to restaurant permissions

--- backend/src/clients/clients.module.ts
+++ backend/src/clients/clients.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { RestaurantModule } from '../restaurant/restaurant.module';
 import { Client } from './entities/client.entity';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
-@Module({ imports:[TypeOrmModule.forFeature([Client])], controllers:[ClientsController], providers:[ClientsService], exports:[ClientsService] })
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Client]), RestaurantModule],
+  controllers: [ClientsController],
+  providers: [ClientsService],
+  exports: [ClientsService],
+})
 export class ClientsModule {}
