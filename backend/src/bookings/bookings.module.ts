b06085c947e55616dcfedc127ff5c5f8e536dec6
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-23 05:44:25 +0300

    Register future booking calendar

--- backend/src/bookings/bookings.module.ts
+++ backend/src/bookings/bookings.module.ts
@@ -7,6 +7,8 @@ import { NotificationsModule } from '../notifications/notifications.module';
 import { Restaurant } from '../restaurant/entities/restaurant.entity';
 import { TableEntity } from '../tables/entities/table.entity';
 
+import { BookingCalendarController } from './booking-calendar.controller';
+import { BookingCalendarService } from './booking-calendar.service';
 import { BookingExpirationService } from './booking-expiration.service';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
@@ -30,8 +32,13 @@ import { GuestReview } from './entities/guest-review.entity';
     LogsModule,
     NotificationsModule,
   ],
-  controllers: [BookingsController],
-  providers: [BookingsService, GuestBookingsService, BookingExpirationService],
+  controllers: [BookingsController, BookingCalendarController],
+  providers: [
+    BookingsService,
+    GuestBookingsService,
+    BookingExpirationService,
+    BookingCalendarService,
+  ],
   exports: [BookingsService, GuestBookingsService],
 })
 export class BookingsModule {}
