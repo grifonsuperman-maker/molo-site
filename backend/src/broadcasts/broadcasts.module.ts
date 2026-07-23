94b4ff67d9fbe28687719657ead6393ead09624b
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-24 00:04:05 +0300

    Wire Telegram delivery into broadcasts module

--- backend/src/broadcasts/broadcasts.module.ts
+++ backend/src/broadcasts/broadcasts.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Broadcast } from './entities/broadcast.entity';
+
 import { Client } from '../clients/entities/client.entity';
+import { LogsModule } from '../logs/logs.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { BroadcastsController } from './broadcasts.controller';
 import { BroadcastsService } from './broadcasts.service';
-import { LogsModule } from '../logs/logs.module';
-@Module({ imports:[TypeOrmModule.forFeature([Broadcast,Client]), LogsModule], controllers:[BroadcastsController], providers:[BroadcastsService], exports:[BroadcastsService] })
+import { Broadcast } from './entities/broadcast.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Broadcast, Client]),
+    LogsModule,
+    NotificationsModule,
+  ],
+  controllers: [BroadcastsController],
+  providers: [BroadcastsService],
+  exports: [BroadcastsService],
+})
 export class BroadcastsModule {}
