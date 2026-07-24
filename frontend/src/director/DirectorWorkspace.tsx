d5b88f31d4ca962757759cbd72726f1d8af315c4
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-24 08:52:56 +0300

    Expose Syrve wizard in Director workspace

--- frontend/src/director/DirectorWorkspace.tsx
+++ frontend/src/director/DirectorWorkspace.tsx
@@ -1,10 +1,12 @@
 import CompactDirectorPanel from './CompactDirectorPanel';
 import DirectorAuthGate from './DirectorAuthGate';
+import SyrveIntegrationDock from './SyrveIntegrationDock';
 
 export default function DirectorWorkspace() {
   return (
     <DirectorAuthGate>
       <CompactDirectorPanel />
+      <SyrveIntegrationDock />
     </DirectorAuthGate>
   );
 }
