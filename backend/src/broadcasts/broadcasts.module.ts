4eeb5537c4a2d5ef05936e009bbe4ad6902ca529
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-25 21:59:08 +0300

    Підключити права Директора до розсилок

--- backend/src/broadcasts/broadcasts.module.ts
+++ backend/src/broadcasts/broadcasts.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from '../clients/entities/client.entity';
 import { LogsModule } from '../logs/logs.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { RestaurantModule } from '../restaurant/restaurant.module';
 import { BroadcastsController } from './broadcasts.controller';
 import { BroadcastsService } from './broadcasts.service';
 import { Broadcast } from './entities/broadcast.entity';
@@ -13,6 +14,7 @@ import { Broadcast } from './entities/broadcast.entity';
     TypeOrmModule.forFeature([Broadcast, Client]),
     LogsModule,
     NotificationsModule,
+    RestaurantModule,
   ],
   controllers: [BroadcastsController],
   providers: [BroadcastsService],
