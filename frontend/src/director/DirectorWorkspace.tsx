2b68752fbce51c003d2510ca72d9e4c953d9d37f
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-07-26 12:55:19 +0300

    Use premium Director panel

--- frontend/src/director/DirectorWorkspace.tsx
+++ frontend/src/director/DirectorWorkspace.tsx
@@ -1,12 +1,12 @@
-import CompactDirectorPanel from './CompactDirectorPanel';
 import DirectorAccessSettingsDock from './DirectorAccessSettingsDock';
 import DirectorAuthGate from './DirectorAuthGate';
+import PremiumDirectorPanel from './PremiumDirectorPanel';
 import SyrveIntegrationDock from './SyrveIntegrationDock';
 
 export default function DirectorWorkspace() {
   return (
     <DirectorAuthGate>
-      <CompactDirectorPanel />
+      <PremiumDirectorPanel />
       <DirectorAccessSettingsDock />
       <SyrveIntegrationDock />
     </DirectorAuthGate>
