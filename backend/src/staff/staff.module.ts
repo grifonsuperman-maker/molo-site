690e4710278aa0665a097fa20b52aec1645f10f1
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-11 15:54:31 +0300

    Wire Telegram staff linking service

--- backend/src/staff/staff.module.ts
+++ backend/src/staff/staff.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { RestaurantModule } from '../restaurant/restaurant.module';
 import { StaffBootstrapService } from './staff-bootstrap.service';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
+import { TelegramStaffLinkService } from './telegram-staff-link.service';
 import { Staff } from './entities/staff.entity';
 import { StaffShiftEvent } from './entities/staff-shift-event.entity';
 
@@ -13,10 +15,15 @@ import { StaffShiftEvent } from './entities/staff-shift-event.entity';
   imports: [
     TypeOrmModule.forFeature([Staff, StaffShiftEvent]),
     AuthModule,
+    NotificationsModule,
     RestaurantModule,
   ],
   controllers: [StaffController],
-  providers: [StaffService, StaffBootstrapService],
-  exports: [StaffService],
+  providers: [
+    StaffService,
+    StaffBootstrapService,
+    TelegramStaffLinkService,
+  ],
+  exports: [StaffService, TelegramStaffLinkService],
 })
 export class StaffModule {}
