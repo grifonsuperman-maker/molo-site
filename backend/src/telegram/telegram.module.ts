ff82ed85205d3f537db8e6141d5b961e975d98fd
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-12 12:26:30 +0300

    Inject staff repository into Telegram callbacks

--- backend/src/telegram/telegram.module.ts
+++ backend/src/telegram/telegram.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { BookingsModule } from '../bookings/bookings.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RestaurantModule } from '../restaurant/restaurant.module';
+import { Staff } from '../staff/entities/staff.entity';
 import { TelegramWebhookController } from './telegram-webhook.controller';
 import { TelegramWebhookService } from './telegram-webhook.service';
-import { BookingsModule } from '../bookings/bookings.module';
-import { RestaurantModule } from '../restaurant/restaurant.module';
-import { NotificationsModule } from '../notifications/notifications.module';
-@Module({ imports:[BookingsModule,RestaurantModule,NotificationsModule], controllers:[TelegramWebhookController], providers:[TelegramWebhookService] })
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Staff]),
+    BookingsModule,
+    RestaurantModule,
+    NotificationsModule,
+  ],
+  controllers: [TelegramWebhookController],
+  providers: [TelegramWebhookService],
+})
 export class TelegramModule {}
