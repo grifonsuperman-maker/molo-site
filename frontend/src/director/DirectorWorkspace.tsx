938a553fa67c00a364aeb368ca79199f258cb114
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-12 16:18:57 +0300

    Show Director site controls in Site tab

--- frontend/src/director/DirectorWorkspace.tsx
+++ frontend/src/director/DirectorWorkspace.tsx
@@ -1,5 +1,6 @@
 import DirectorAccessSettingsDock from './DirectorAccessSettingsDock';
 import DirectorAuthGate from './DirectorAuthGate';
+import DirectorSiteControlsDock from './DirectorSiteControlsDock';
 import PremiumDirectorPanel from './PremiumDirectorPanel';
 import SyrveIntegrationDock from './SyrveIntegrationDock';
 import TelegramStaffInvitePanel from '../staff/TelegramStaffInvitePanel';
@@ -8,6 +9,7 @@ export default function DirectorWorkspace() {
   return (
     <DirectorAuthGate>
       <PremiumDirectorPanel />
+      <DirectorSiteControlsDock />
       <TelegramStaffInvitePanel audience="director" />
       <DirectorAccessSettingsDock />
       <SyrveIntegrationDock />
