bdd89c1bf155b0b9b3017d915cbcb6c94b4dd7bd
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-15 14:39:19 +0300

    Зареєструвати waiter assignment migration перед lifecycle

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,6 +17,7 @@ import { MapModule } from './map/map.module';
 import { CreateStaffPinAttempts2026081400010 } from './migrations/2026081400010-CreateStaffPinAttempts';
 import { UpgradeStaffPinAttemptsPerAttempt2026081400020 } from './migrations/2026081400020-UpgradeStaffPinAttemptsPerAttempt';
 import { CreateWaiterCalls2026081500010 } from './migrations/2026081500010-CreateWaiterCalls';
+import { AddWaiterCallAssignmentActive2026081500015 } from './migrations/2026081500015-AddWaiterCallAssignmentActive';
 import { CloseInactiveWaiterCalls2026081500020 } from './migrations/2026081500020-CloseInactiveWaiterCalls';
 import { NotificationsModule } from './notifications/notifications.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
@@ -33,6 +34,7 @@ const staffPinMigrationOptions = {
     CreateStaffPinAttempts2026081400010,
     UpgradeStaffPinAttemptsPerAttempt2026081400020,
     CreateWaiterCalls2026081500010,
+    AddWaiterCallAssignmentActive2026081500015,
     CloseInactiveWaiterCalls2026081500020,
   ],
 };
