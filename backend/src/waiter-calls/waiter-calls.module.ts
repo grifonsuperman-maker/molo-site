3e25267d27d171c00fd2e02cc11daa13783e5a8a
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-15 11:56:03 +0300

    Підключити сховище викликів офіціанта

--- backend/src/waiter-calls/waiter-calls.module.ts
+++ backend/src/waiter-calls/waiter-calls.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BookingHistory } from '../bookings/entities/booking-history.entity';
 import { Booking } from '../bookings/entities/booking.entity';
+import { WaiterCallRecord } from './entities/waiter-call.entity';
 import { WaiterCallsController } from './waiter-calls.controller';
 import { WaiterCallsService } from './waiter-calls.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, BookingHistory])],
+  imports: [TypeOrmModule.forFeature([Booking, BookingHistory, WaiterCallRecord])],
   controllers: [WaiterCallsController],
   providers: [WaiterCallsService],
   exports: [WaiterCallsService],
