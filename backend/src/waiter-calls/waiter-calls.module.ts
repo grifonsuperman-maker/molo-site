369d0c3f8c5104654b94d079058dd20d414e10c0
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-20 17:05:58 +0300

    Підключити Telegram push для викликів Офіціанта

--- backend/src/waiter-calls/waiter-calls.module.ts
+++ backend/src/waiter-calls/waiter-calls.module.ts
@@ -3,14 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BookingHistory } from '../bookings/entities/booking-history.entity';
 import { Booking } from '../bookings/entities/booking.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { Staff } from '../staff/entities/staff.entity';
 import { WaiterCallRecord } from './entities/waiter-call.entity';
+import { WaiterCallTelegramNotifierService } from './waiter-call-telegram-notifier.service';
 import { WaiterCallsController } from './waiter-calls.controller';
 import { WaiterCallsService } from './waiter-calls.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, BookingHistory, WaiterCallRecord])],
+  imports: [
+    TypeOrmModule.forFeature([Booking, BookingHistory, WaiterCallRecord, Staff]),
+    NotificationsModule,
+  ],
   controllers: [WaiterCallsController],
-  providers: [WaiterCallsService],
+  providers: [WaiterCallsService, WaiterCallTelegramNotifierService],
   exports: [WaiterCallsService],
 })
 export class WaiterCallsModule {}
