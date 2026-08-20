bb8563d3846b28cc8cb151e5735ef71c114fb284
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-20 13:22:32 +0300

    Підключити Telegram-пульт офіціанта

--- backend/src/telegram/telegram.module.ts
+++ backend/src/telegram/telegram.module.ts
@@ -4,12 +4,22 @@ import { BookingsModule } from '../bookings/bookings.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { RestaurantModule } from '../restaurant/restaurant.module';
 import { StaffModule } from '../staff/staff.module';
+import { TablesModule } from '../tables/tables.module';
+import { WaiterCallsModule } from '../waiter-calls/waiter-calls.module';
+import { TelegramWaiterMenuService } from './telegram-waiter-menu.service';
 import { TelegramWebhookController } from './telegram-webhook.controller';
 import { TelegramWebhookService } from './telegram-webhook.service';
 
 @Module({
-  imports: [BookingsModule, RestaurantModule, NotificationsModule, StaffModule],
+  imports: [
+    BookingsModule,
+    RestaurantModule,
+    NotificationsModule,
+    StaffModule,
+    TablesModule,
+    WaiterCallsModule,
+  ],
   controllers: [TelegramWebhookController],
-  providers: [TelegramWebhookService],
+  providers: [TelegramWebhookService, TelegramWaiterMenuService],
 })
 export class TelegramModule {}
