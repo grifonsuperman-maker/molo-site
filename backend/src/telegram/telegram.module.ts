9719c1cb08d239b59d9a673b34984f257192168c
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-20 15:23:00 +0300

    Підключити bulk lookup призначень Telegram

--- backend/src/telegram/telegram.module.ts
+++ backend/src/telegram/telegram.module.ts
@@ -6,6 +6,7 @@ import { RestaurantModule } from '../restaurant/restaurant.module';
 import { StaffModule } from '../staff/staff.module';
 import { TablesModule } from '../tables/tables.module';
 import { WaiterCallsModule } from '../waiter-calls/waiter-calls.module';
+import { TelegramWaiterAssignmentLookupService } from './telegram-waiter-assignment-lookup.service';
 import { TelegramWaiterMenuResolvedService } from './telegram-waiter-menu-resolved.service';
 import { TelegramWaiterMenuService } from './telegram-waiter-menu.service';
 import { TelegramWebhookController } from './telegram-webhook.controller';
@@ -23,6 +24,7 @@ import { TelegramWebhookService } from './telegram-webhook.service';
   controllers: [TelegramWebhookController],
   providers: [
     TelegramWebhookService,
+    TelegramWaiterAssignmentLookupService,
     TelegramWaiterMenuResolvedService,
     {
       provide: TelegramWaiterMenuService,
