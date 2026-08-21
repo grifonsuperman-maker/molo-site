d72a84f5c7747fbfbbf64e2efc0c1e7a757e0b9c
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-21 22:23:34 +0300

    Підключити Telegram push Кальянника

--- backend/src/hookah-calls/hookah-calls.module.ts
+++ backend/src/hookah-calls/hookah-calls.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Booking } from "../bookings/entities/booking.entity";
+import { NotificationsModule } from "../notifications/notifications.module";
 import { Staff } from "../staff/entities/staff.entity";
 import { Restaurant } from "../restaurant/entities/restaurant.entity";
 import { WaiterCallsModule } from "../waiter-calls/waiter-calls.module";
 import { HookahCall } from "./entities/hookah-call.entity";
+import { HookahCallTelegramNotifierService } from "./hookah-call-telegram-notifier.service";
 import { HookahCallsController } from "./hookah-calls.controller";
 import { HookahCallsService } from "./hookah-calls.service";
 import { HookahGuestAccessService } from "./hookah-guest-access.service";
@@ -12,10 +14,15 @@ import { HookahGuestAccessService } from "./hookah-guest-access.service";
 @Module({
   imports: [
     TypeOrmModule.forFeature([HookahCall, Booking, Staff, Restaurant]),
+    NotificationsModule,
     WaiterCallsModule,
   ],
   controllers: [HookahCallsController],
-  providers: [HookahCallsService, HookahGuestAccessService],
+  providers: [
+    HookahCallsService,
+    HookahGuestAccessService,
+    HookahCallTelegramNotifierService,
+  ],
   exports: [HookahCallsService],
 })
 export class HookahCallsModule {}
