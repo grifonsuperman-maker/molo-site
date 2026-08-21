8909d11b89775aeb29110bef09d74829f94195e0
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-08-22 00:13:48 +0300

    Підключити Telegram-пульт Адміністратора

--- backend/src/telegram/telegram.module.ts
+++ backend/src/telegram/telegram.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { BookingsModule } from '../bookings/bookings.module';
+import { BroadcastsModule } from '../broadcasts/broadcasts.module';
 import { HookahCallsModule } from '../hookah-calls/hookah-calls.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { RestaurantModule } from '../restaurant/restaurant.module';
 import { StaffModule } from '../staff/staff.module';
 import { TablesModule } from '../tables/tables.module';
 import { WaiterCallsModule } from '../waiter-calls/waiter-calls.module';
+import { TelegramAdminMenuService } from './telegram-admin-menu.service';
 import { TelegramHookahMenuService } from './telegram-hookah-menu.service';
 import { TelegramWaiterAssignmentLookupService } from './telegram-waiter-assignment-lookup.service';
 import { TelegramWaiterMenuResolvedService } from './telegram-waiter-menu-resolved.service';
@@ -17,6 +19,7 @@ import { TelegramWebhookService } from './telegram-webhook.service';
 @Module({
   imports: [
     BookingsModule,
+    BroadcastsModule,
     HookahCallsModule,
     RestaurantModule,
     NotificationsModule,
@@ -27,6 +30,7 @@ import { TelegramWebhookService } from './telegram-webhook.service';
   controllers: [TelegramWebhookController],
   providers: [
     TelegramWebhookService,
+    TelegramAdminMenuService,
     TelegramHookahMenuService,
     TelegramWaiterAssignmentLookupService,
     TelegramWaiterMenuResolvedService,
