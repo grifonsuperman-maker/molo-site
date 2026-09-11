6a273962f0768087b3c62b4e4ec20a5f49c03eaa
grifonsuperman-maker <grifonsuperman@gmail.com>
2026-09-11 03:11:23 +0300

    Дозволити валідатору побачити зайву цифру телефону

--- frontend/src/guest/GuestApp.tsx
+++ frontend/src/guest/GuestApp.tsx
@@ -1934,7 +1934,7 @@ export default function GuestApp() {
                 inputMode="tel"
                 autoComplete="tel"
                 placeholder="+380 (__) ___-__-__"
-                maxLength={19}
+                maxLength={20}
                 value={form.phone}
                 onChange={(event) =>
                   setForm({ ...form, phone: formatUkrainePhoneInput(event.target.value) })
